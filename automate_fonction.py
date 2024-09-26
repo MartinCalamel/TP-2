4f0b75979acfc23ad954296966cd97841ed6b3e4
MartinCalamel <martin.calamel@cpe.fr>
2024-09-26 11:19:22 +0200

    add file fonction and dico file

--- automate_fonction.py
+++ automate_fonction.py
@@ -2,26 +2,8 @@
 Author: Martin Calamel
 Created: 2024-09-26
 Description: automate qui vérifie la structure grammatical d'une phrase 
-TODO: dictionnaire en fichier:
-        - fonction pour lire le fichier
-        - fonction pour créer le dictionnaire
-        - fonction pour modifier le dictionnaire
+TODO: 
 """
-
-#### initialisation des variables
-
-dictionnaire = {"le" : 0, "la" : 0, "chat" : 2, "souris" : 2, "martin" : 3,
-"mange" : 4, "la" : 0, "petite" : 1, "joli" : 1, "grosse" : 1,
-"bleu" : 1, "verte" : 1, "dort" : 4,"julie" : 3, "jean" : 3, "." : 5}
-
-matrice_etats = [[1, 8, 8, 4, 8, 8],
-                 [8, 1, 2, 8, 8, 8],
-                 [8, 2, 8, 8, 3, 8],
-                 [5, 8, 8, 7, 8, 9],
-                 [8, 8, 8, 8, 3, 8],
-                 [8, 5, 6, 8, 8, 8],
-                 [8, 6, 8, 8, 8, 9],
-                 [8, 8, 8, 8, 8, 9]]
 
 #### fonction utiles
 
@@ -51,6 +33,7 @@ def format_phrase(phrase: str) -> list:
 
     return liste_mot
 
+
 def check_in_dico(dico: dict, key):
     """
     vérifie la presence d'une clef dans un dictionnaire
@@ -59,6 +42,76 @@ def check_in_dico(dico: dict, key):
     output: presence de la clef (bool)
     """
     return key in dico.keys()
+
+
+def check_file(file_name: str) -> bool:
+    """
+    Fonction pour vérifier que le fichier est bien existant
+
+    #input : nom du fichier a vérifier (str)
+    #output : existence du fichier (bool)
+    """
+    if type(file_name) == str : 
+        try:
+            file_data = open(file_name)
+            file_data.close()
+            return True
+        
+        except FileNotFoundError:
+            print("le fichier n'existe pas")
+            return False
+
+    else:
+        print("mauvaise saisie du nom de fichier")
+        return False
+
+def read_file(file_name: str) -> list:
+    """
+    renvoi les données du fichier spécifier après avoir vérifier sont existence
+
+    #input : nom du fichier (str)
+    #output : données contenue dans le fichier (list)
+    """
+    if check_file(file_name):
+
+        file = open(file_name,"r")
+        content = file.read()
+        content = content.split("\n") # sépare les element de content par ;
+        data = [ i.split(";") for i in content]
+        file.close()
+
+        return data
+    
+def write_file(file_name: str, expression: str) -> None:
+    if check_file(file_name):
+        file = open(file_name,"a")
+        file.write(expression)
+        file.close()
+
+def make_dico_file(file: str) -> dict:
+    """
+    fait un dictionnaire a partir d'un fichier\n
+    on part du principe que le fichier est sous le bon format pour travailler avec les dico
+
+    input: nom du fichier où est stocker le dictionnaire (str)
+    output: dictionnaire (dict)
+    """
+
+    data = read_file(file)
+    print(data)
+    dico = {i[0] : int(i[1]) for i in data}
+    
+    return dico
+
+def add_to_dico_file(key: str, value: int, file: str) -> None:
+    """
+    fonction qui ajoute un ensemble clef valeur au fichier du dictionnaire
+
+    input: clef (str), valeur (int), fichier du dictionnaire (str)
+    output: rien
+    """
+    expression="\n"+key+";"+str(value)
+    write_file(file, expression)
 
 #### fonction automates
 
@@ -81,6 +134,8 @@ def verif_phrase(phrase: str) -> int:
     input: phrase a verifier (str)
     output: 0 si bien passer (int) 
     """
+    global dictionnaire
+
     liste_mots=format_phrase(phrase)
     etat= 0
     for i in liste_mots:
@@ -96,13 +151,62 @@ def verif_phrase(phrase: str) -> int:
             choix = input("voulez-vous l'ajouter ? (Y/N) : ")
             if choix.upper() == "Y":
                 classe_mot=int(input("classe du mot:\n 0 = article\n 1 = adjectif\n 2 = Nom commun\n 3 = Nom Propre\n 4 = verbe\n>>>  "))
-                dictionnaire[i]=classe_mot
+                add_to_dico_file(i, classe_mot, dico_file)
+                dictionnaire = make_dico_file(dico_file)
                 verif_phrase(phrase)
     return 0
 
 
+#### initialisation des variables
+
+dico_file="dictionnaire.txt"
+
+dictionnaire = make_dico_file(dico_file)
+
+matrice_etats = [[1, 8, 8, 4, 8, 8],
+                 [8, 1, 2, 8, 8, 8],
+                 [8, 2, 8, 8, 3, 8],
+                 [5, 8, 8, 7, 8, 9],
+                 [8, 8, 8, 8, 3, 8],
+                 [8, 5, 6, 8, 8, 8],
+                 [8, 6, 8, 8, 8, 9],
+                 [8, 8, 8, 8, 8, 9]]
+
+
 
 if __name__ == "__main__":
+
+    # fonction write_file
+
+    # write_file("dictionnaire.txt", "\nles;0")
+
+    # fonction make_dico_file
+    print(make_dico_file("dictionnaire.txt"))
+
+    # fonction format_phrase
+
+    print(format_phrase("Phrase avec; des truc!! pas beau du tout."))
+
+    # fonction check_in_dico
+
+    print("check_in_dico -> clef OK ",check_in_dico({0:1,2:3},0))
+    print("check_in_dico -> clef OK ",check_in_dico({0:1,2:3},1))
+    
+
+    # fonction check_file
+
+    print("check_file -> existence OK : ", check_file("dictionnaire.txt"))
+    print("check_file -> existence NO : ", check_file("blabla.txt"))
+    print("check_file -> mauvais format : ", check_file(12))
+    print("\n\n")
+
+    # fonction read_file
+
+    print("read_file -> existence OK : ", read_file("dictionnaire.txt"))
+    print("read_file -> existence NO : ", read_file("blabla.txt"))
+    print("read_file -> mauvais format : ", read_file(12))
+    print("\n\n")
+
 
     # phrase qui devrait être correctes
 
